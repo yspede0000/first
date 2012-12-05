f162919612fdfcb050bc7e46e743fa1d05d1dab2
yspede0000 <yspede1@gmail.com>
2012-12-05 11:45:59 +0100

    Update radiov1/radiov1/main.c

--- radiov1/radiov1/main.c
+++ radiov1/radiov1/main.c
@@ -1,61 +1,66 @@
 #include <msp430g2231.h>
 #include "i2c.h"
 
-unsigned char i2cAddress = 0x40;
-int i;
-
-void setFreq() //~4158 Hz
-{
-  unsigned int oct = 2;
-  unsigned int dac = 1;
-
-  //f=2^oct*2078/(2-dac/1024)
-  unsigned int reg = oct << 12 | dac << 2;
-
-  unsigned char high = (reg >> 8) & 0xff;
-  unsigned char low = reg & 0xff;
+unsigned char expander = 0x20; //address on mcp23016
+unsigned char dir0 = 0x06; // direction register Port0
+unsigned char port0 = 0x00; // Port0; can be directly written/read.
 
 
-}
+
 
 void main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;
+
+	   i2c_init(); // startup i2c
+
+ /*
   P1DIR |= 0x1C;
-// set port 1.2 HIGH (RST)
-P1OUT |= 0x04;
-
-// set port 1.3 HIGH (VDD 3.3V)
-P1OUT |= 0x08;
-
-for (i=1; i<30000; i=i+1) {}  //Delay
-
-// delay 5ms
-
 // set port 1.2 LOW (RST)
-P1OUT &= ~0x04;
+P1OUT &= 0x04;
+
+// set port 1.3 LOW (VDD 3.3V)
+P1OUT &= 0x08;
 
 // set port 1.4 HIGH (SEN)
 P1OUT |= 0x10;
 
 
-i2c_init();
-setFreq();
-i2c_write8(i2cAddress << 1);
-//while(1) {
-  i2c_write8(0x20); //command to control B0
-  i2c_write8(0x7f); // port B0 is on
 
-//}
-  i2c_stop();
+// delay 1ms
+delay(201);
+
+// set port 1.2 High (RST)
+P1OUT &= ~0x04;
+
+// delay 100ms
+
+// set port 1.3 HIGH (VDD 3.3V)
+P1OUT |= 0x08;
+*/
+
+
+
+i2c_start();
+i2c_write8(expander << 1);
+i2c_write8(dir0);
+i2c_write8(0x00); // all port0 output
+i2c_stop();
+
+i2c_start();
+i2c_write8(expander << 1);
+i2c_write8(port0);
+i2c_write8(0xFF); // all port0 high
+i2c_stop();
+
+
 /*
- * CMD 0x01
- * ARG1 0x00
- * ARG2 0x05
+ * CMD 0x01 // point on startup reg
+ * ARG1 0x10 // send use crystal
+ * ARG2 0x05 // use analog output.
  * status 0x80 reply status (clear to send high)
  *
  *
  */
 
 }
-
